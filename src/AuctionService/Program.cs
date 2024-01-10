8391b7c02c25f0687036bc9f3b29abef10293973
CeeObi <140174587+CeeObi@users.noreply.github.com>
2024-01-10 14:22:55 +1100

    implement DTO and mapping entities to dto

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddDbContext<AuctionDbContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
@@ -26,5 +27,14 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+try
+{
+    DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{    
+    Console.WriteLine(e);
+}
 
 app.Run();
