6f2bde46f266c828ed8b3500ccce9d154de3d8c1
CeeObi <140174587+CeeObi@users.noreply.github.com>
2024-01-11 11:15:15 +1100

    implement mongodb setup for searchservice

---
+++ src/SearchService/Model/Item.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Entities;
+
+namespace SearchService.Item;
+
+public class Item : Entity //has same properties as the Auction DTO but derieved from mgDb class called Entity which provides its own id, hence no id property.
+{
+    public int ReservePrice {get; set;}
+    public string Seller {get; set;}
+    public string Winner {get; set;}
+    public int SoldAmount {get; set;}
+    public int CurrentHighBid {get; set;}
+    public DateTime CreatedAt {get; set;}
+    public DateTime UpdatedAt {get; set;}
+    public DateTime AuctionEnd {get; set;}
+    public string Status {get; set;}
+    public string Make {get; set;}
+    public string Model {get; set;}
+    public string Color {get; set;}
+    public int Year {get; set;}
+    public int Mileage {get; set;}
+    public string ImageUrl {get; set;}
+}
