8f5c3a76771a084f2a076ef293af9aac3e80457c
CeeObi <140174587+CeeObi@users.noreply.github.com>
2024-01-17 03:16:53 +1100

    update identity-svc container

--- src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
+++ src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
@@ -12,8 +12,8 @@ public class Index : PageModel
     public ViewModel View { get; set; }
         
     public async Task<IActionResult> OnGet()
-    {
-        var localAddresses = new string[] { "::ffff:172.18.0.1", "127.0.0.1", "::1", HttpContext.Connection.LocalIpAddress.ToString() };//previous line for debug claims
+    {   
+        var localAddresses = new string[] { "::ffff:172.20.0.1","::ffff:172.18.0.1", "127.0.0.1", "::1", HttpContext.Connection.LocalIpAddress.ToString() };//previous line for debug claims
         if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress.ToString()))
         {
             return NotFound();
