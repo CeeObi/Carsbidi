ac1bd2e4472c70a6e5db719240768fdc92688aeb
CeeObi <140174587+CeeObi@users.noreply.github.com>
2024-01-19 12:40:56 +1100

    debug request for session pg

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -24,12 +24,11 @@ export async function updateAuctionTest() {
   const res =  await fetch("http://localhost:6001/auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c",{
     method: 'PUT',
     headers: {
-      "content": "application/json",
+      "Content-Type": "application/json",
       "Authorization": "Bearer " + token?.access_token
     },
     body: JSON.stringify(data)
   });
-  
   if (!res.ok) return {status: res.status, message: res.statusText}
   return res.statusText
 }
--- frontend/web-app/app/session/page.tsx
+++ frontend/web-app/app/session/page.tsx
@@ -20,7 +20,7 @@ async function session() {
             <h3 className='text-lg'>Session Data</h3>
             <pre>{JSON.stringify(session,null,2)}</pre>
         </div>
-        <AuthTest />
+        <AuthTest /> 
         <div className="bg-green-200 border-2 border-blue-500 mt-4">
             <h3 className='text-lg'>Token Data</h3>
             <pre className='overflow-auto'>{JSON.stringify(token,null,2)}</pre>
