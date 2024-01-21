db93a044a151f1e21cf6678d7c92168501de4377
CeeObi <140174587+CeeObi@users.noreply.github.com>
2024-01-21 23:24:15 +1100

    implement initial bid service setup, add create bid and get bid

--- frontend/web-app/app/auctions/create/page.tsx
+++ frontend/web-app/app/auctions/create/page.tsx
@@ -6,7 +6,7 @@ function Create() {
   return (
     <div className='mx-auto max-w-[75%] shadow-lg p-10 bg-white rounded-lg'>
             <Headings title='Sell Your Car' subtitle="Please enter the details of your car" />
-            <AuctionForm/>
+            <AuctionForm/> 
     </div>
   )
 }
