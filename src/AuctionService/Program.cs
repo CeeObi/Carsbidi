515119776d0474f3644464cf7a112fd746e66f6c
CeeObi <140174587+CeeObi@users.noreply.github.com>
2024-01-22 21:29:59 +1100

    implement GrpcAuctionService

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,5 +1,6 @@
 using AuctionService;
 using AuctionService.Data;
+using AuctionService.Services;
 using Contracts;
 using MassTransit;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -49,12 +50,15 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
     options.TokenValidationParameters.NameClaimType = "username";
 });
 
+builder.Services.AddGrpc();
+
 var app = builder.Build();
 
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapGrpcService<GrpcAuctionService>();
 
 try
 {
